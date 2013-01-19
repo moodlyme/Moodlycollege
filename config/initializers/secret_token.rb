05e72432a48a7e9d99c938712330cfd4d1b47e99
Kevin Seo <moodlyme@gmail.com>
2013-01-19 13:13:13 -0600

    Intial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Moodlycollege::Application.config.secret_token = '024e767123333fcb930ee02b3ad708b8e34d9c30ad14cab36f50f35fb33290d4d52d91fc18add7f8dcab83297f9924cd25121363dae406f03a95a1f7a0cec03a'
